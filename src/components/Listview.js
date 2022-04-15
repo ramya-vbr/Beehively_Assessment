a666805d2363d6cd68168c4e99fb11c475d18760
ramya-vbr <vramya57@gmail.com>
2022-04-15 20:15:46 +0400

    first commit

---
+++ src/components/Listview.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Header, Icon, Modal, Input } from 'semantic-ui-react';
+import './listview.css';
+
+function Listview() {
+    const [open, setOpen] = useState(false);
+    const [favorite, setFavorite] = useState([]);
+    const [searchdata, setSearchData] = useState("");
+    const [moredetails, setMoreDetails] = useState();
+    const [loader, setLoader] = useState(true);
+    const [noDataFound, setNoDataFound] = useState(false);
+
+    useEffect(() => {
+        setLoader(true);
+        const url = 'https://api.github.com/search/repositories?q=stars:>1+language:javascript&sort=stars&order=desc';
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((parsedData) => {
+                setLoader(false);
+                setFavorite(parsedData.items);
+            })
+    }, [])
+
+    useEffect(()=>{
+        let findNoData = favorite.find((li)=>li.name.includes(searchdata));
+        setNoDataFound(findNoData || searchdata === "" ? false : true)
+    },[searchdata])
+
+    const openmodal = (datas) => {
+        setOpen(true);
+        setMoreDetails(datas);
+
+    }
+    const handleSearch = (e) => {
+        setSearchData(e.target.value);
+    }
+
+    return (
+        <div className='style-inputbox'>
+            {loader ? <div>Loading...</div> :
+            <>
+            <Input icon='search' placeholder='Search by Github name' onChange={handleSearch} />
+            <div className='full-ctn'>
+                {noDataFound && <h3 className='nodata-found'>No Result Found</h3>}
+                {favorite.map((data, index) => {
+                    return (
+                        data.name.includes(searchdata) || searchdata === "" ?
+                            <Card key={index}>
+                                <Card.Content header={data.name} />
+                                <Card.Content className="descrip-ctn" description={data.description} />
+                                <div className='langbar-style'>
+                                    <div>{data.language}</div>
+                                    <a target="_blank" href={data.owner.html_url}>Link</a>
+                                </div>
+                                <Card.Content extra className='more-container'>
+                                    <div className='login-style'>{data.owner.login}</div>
+                                    <Modal
+                                        closeIcon
+                                        open={open}
+                                        trigger={<Button primary>More</Button>}
+                                        onClose={() => setOpen(false)}
+                                        onOpen={() => openmodal(data)}
+                                    >
+                                        <Header icon='archive' content=' Repository Details' />
+                                        <Modal.Content className='modal-style'>
+                                            <p><span className='design-text'>Name:</span>{moredetails?.name}</p>
+                                            <p><span className='design-text'>Full Name:</span>{moredetails?.full_name}</p>
+                                            <p><span className='design-text'>Language:</span>{moredetails?.language}</p>
+                                            <p><span className='design-text'>Login:</span>{moredetails?.owner.login}</p>
+                                            <p><span className='design-text'>Type:</span>{moredetails?.owner.type}</p>
+                                            <p><span className='design-text'>url:</span>{moredetails?.owner.url}</p>
+                                            <p><span className='design-text'>License:</span>{moredetails?.license.name}</p>
+                                            <p><span className='design-text'>Description:</span>{moredetails?.description}</p>
+                                        </Modal.Content>
+                                        <Modal.Actions>
+                                            <Button color='red' onClick={() => setOpen(false)}>
+                                                Close
+                                            </Button>
+                                        </Modal.Actions>
+                                    </Modal>
+                                </Card.Content>
+                            </Card> : ""
+                    )
+                })
+                }
+            </div>
+            </>}
+        </div>
+    )
+}
+export default Listview;
